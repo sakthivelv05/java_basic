38f54adf7602f3283c63b1dd8efde302fb07be15
sakthivelv05 <sakthivelvelmurugan7815@gmail.com>
2026-04-28 09:31:23 +0530

    day 1 code

--- day2.java
+++ day2.java
@@ -1,3 +1,29 @@
-public class day2 {
+class day2 {
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        System.out.println("a+b "+ (a+b));
+        System.out.println("a-b "+ (a-b));
+        System.out.println("a*b "+ (a*b));       
+        System.out.println("a/b "+ (a/b));
+        System.out.println("a%b "+ (a%b));
+        
+        System.out.println("a++ "+ (a++));
+        System.out.println("a-- "+ (a--));   
+        System.out.println("b++ "+ (b++));
+        System.out.println("b-- "+ (b--));
+
+        System.out.println("a && b "+ (a & b));
+        System.out.println("a || b "+ (a |b));
+
+        System.out.println("a > b "+ (a > b));
+        System.out.println("a < b "+ (a < b));
+        System.out.println("a == b "+ (a == b));
+
+        int max=(a>b) ? a : b;
+        System.out.println("max is "+ max);
+        
+        System.out.println((a>b) ? "a is greater" : "b is greater");
     
+    }
 }
