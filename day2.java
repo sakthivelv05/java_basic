790c09689a08628b7eab9bfd68a37cf7f181e832
sakthivelv05 <sakthivelvelmurugan7815@gmail.com>
2026-04-28 14:50:19 +0530

    day 2 code

--- day2.java
+++ day2.java
@@ -14,16 +14,25 @@ class day2 {
         System.out.println("b-- "+ (b--));
 
         System.out.println("a && b "+ (a & b));
-        System.out.println("a || b "+ (a |b));
+        System.out.println("a || b "+ (a | b));
+        System.out.println("a ^ b "+ (a ^ b));
+        System.out.println("~a "+ (~a));
+        System.out.println("~b "+ (~b));
+
 
         System.out.println("a > b "+ (a > b));
         System.out.println("a < b "+ (a < b));
         System.out.println("a == b "+ (a == b));
+        System.out.println(a<<1);
+        System.out.println(a>>1);
+    
 
         int max=(a>b) ? a : b;
         System.out.println("max is "+ max);
-        
+
         System.out.println((a>b) ? "a is greater" : "b is greater");
+
+         
     
     }
 }
---
+++ day_2.java
@@ -0,0 +1,23 @@
+class day_2 {
+    String a;
+    public static void main(String[] args){
+        sak o=new sak();
+        o.add();
+        System.out.println(o.a);
+        System.out.println(sak.b);
+       
+
+    }
+
+}
+
+class sak{
+    int a=20;
+    static int b=30;
+    void add(){
+        int a=10;
+        System.out.println(a);
+        
+    }
+
+}
